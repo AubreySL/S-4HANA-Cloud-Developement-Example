30cafded9cabd94a84375bb9f3bc0f7f3ea6819b
songlin.wei <songlin.wei@sap.com>
2018-12-06 09:27:37 +0800

    add correspond logic

--- application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/DeleteAddressCommand.java
+++ application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/DeleteAddressCommand.java
@@ -1,42 +1,36 @@
 package com.sap.cloud.s4hana.examples.addressmgr.commands;
 
-import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
-import com.sap.cloud.sdk.frameworks.hystrix.HystrixUtil;
 import com.sap.cloud.sdk.odatav2.connectivity.ODataDeleteResult;
-import com.sap.cloud.sdk.s4hana.connectivity.ErpCommand;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartnerAddress;
-import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
 import org.slf4j.Logger;
+import com.sap.cloud.sdk.frameworks.hystrix.HystrixUtil;
+import com.sap.cloud.sdk.s4hana.connectivity.ErpCommand;
+import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
 
 public class DeleteAddressCommand extends ErpCommand<Integer> {
-    private static final Logger logger = CloudLoggerFactory.getLogger(DeleteAddressCommand.class);
+	private static final Logger logger = CloudLoggerFactory.getLogger(DeleteAddressCommand.class);
 
-    private final BusinessPartnerService service;
-    private final String businessPartnerId;
-    private final String addressId;
+	private final BusinessPartnerService service;
+	private final String businessPartnerId;
+	private final String addressId;
 
-    public DeleteAddressCommand(final BusinessPartnerService service,
-                                final String businessPartnerId, final String addressId) {
-        super(HystrixUtil.getDefaultErpCommandSetter(
-                DeleteAddressCommand.class,
-                HystrixUtil.getDefaultErpCommandProperties().withExecutionTimeoutInMilliseconds(10000)));
+	public DeleteAddressCommand(final BusinessPartnerService service, final String businessPartnerId,
+			final String addressId) {
+		super(HystrixUtil.getDefaultErpCommandSetter(DeleteAddressCommand.class,
+				HystrixUtil.getDefaultErpCommandProperties().withExecutionTimeoutInMilliseconds(10000)));
+		this.service = service;
+		this.businessPartnerId = businessPartnerId;
+		this.addressId = addressId;
+	}
 
-        this.service = service;
-        this.businessPartnerId = businessPartnerId;
-        this.addressId = addressId;
-    }
+	@Override
+	public Integer run() throws Exception {
+		final BusinessPartnerAddress businessPartnerAddress = BusinessPartnerAddress.builder()
+				.businessPartner(businessPartnerId).addressID(addressId).build();
 
-    @Override
-    protected Integer run() throws Exception {
-        final BusinessPartnerAddress addressToDelete = BusinessPartnerAddress.builder()
-                .businessPartner(businessPartnerId)
-                .addressID(addressId)
-                .build();
-
-        final ODataDeleteResult oDataDeleteResult = service
-                .deleteBusinessPartnerAddress(addressToDelete)
-                .execute();
-
-        return oDataDeleteResult.getHttpStatusCode();
-    }
+		final ODataDeleteResult oDataDeleteResult = service.deleteBusinessPartnerAddress(businessPartnerAddress)
+				.execute();
+		return oDataDeleteResult.getHttpStatusCode();
+	}
 }
